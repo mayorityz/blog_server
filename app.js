41c477f8e57a0b1833624a5c50ce0567e7566979
Mayor <mayority11@gmail.com>
2021-12-12 17:28:57 +0100

    cors

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-import express from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import DB_CONNECTION from "./Utilities/DB.js";
-import users from "./Component/Users/user.route.js";
-import cases from "./Component/Cases/case.route.js";
-import post from "./Component/Blog/Blog.route.js";
-import category from "./Component/Category/category.route.js";
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import DB_CONNECTION from './Utilities/DB.js'
+import users from './Component/Users/user.route.js'
+import cases from './Component/Cases/case.route.js'
+import post from './Component/Blog/Blog.route.js'
+import category from './Component/Category/category.route.js'
 
-const app = express();
-const PORT = process.env.PORT || 8084;
+const app = express()
+const PORT = process.env.PORT || 8084
 
 let ORIGIN = process.env.NODE_ENV
-  ? "https://insidesparkles.surge.sh"
-  : "http://localhost:3000";
+  ? 'https://insidesparkles.surge.sh'
+  : 'http://localhost:3000'
 
-app.use(cookieParser());
+app.use(cookieParser())
 
-app.use(cors({ origin: ORIGIN, credentials: true }));
+app.use(cors())
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: '50mb' }))
 app.use(
-  express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
-);
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
+)
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", ORIGIN);
-  res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+  res.setHeader('Access-Control-Allow-Origin', ORIGIN)
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+})
 
-app.use("/api/v1/users", users);
-app.use("/api/v1/cases", cases);
-app.use("/api/v1/post", post);
-app.use("/api/v1/categories", category);
+app.use('/api/v1/users', users)
+app.use('/api/v1/cases', cases)
+app.use('/api/v1/post', post)
+app.use('/api/v1/categories', category)
 
 app.use((req, res, next) => {
-  res.status(404).send("Sorry can't find that!");
-});
+  res.status(404).send("Sorry can't find that!")
+})
 
 // catch server errors and respond with 500
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 try {
   DB_CONNECTION.then(() => {
-    console.log("listening");
+    console.log('listening')
     app.listen(PORT, () => {
-      console.log("connecting DB ...");
-      console.log(`running on port ${PORT}`);
-    });
-  });
+      console.log('connecting DB ...')
+      console.log(`running on port ${PORT}`)
+    })
+  })
 } catch (error) {
-  console.log(error.message);
-  res.status(500).send(error.message);
+  console.log(error.message)
+  res.status(500).send(error.message)
 }
