f965f813ba6a8338df60de90f679315634d5d3c6
Mayor <mayority11@gmail.com>
2021-12-12 17:30:42 +0100

    cors

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(
 )
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', ORIGIN)
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', '*')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   next()
